ceeed0094674788c84ace272e8a1048484df0506
Julio-06 <jtcesarin1998@gmail.com>
2022-09-17 18:07:33 -0500

    [CLASE 20] Obtener varios parámetros de la URL con @RequestParam y HttpServletRequest

--- demo/src/main/java/com/julio/app/demo/controllers/EjemploParamsController.java
+++ demo/src/main/java/com/julio/app/demo/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.julio.app.demo.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,7 @@ public class EjemploParamsController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("titulo", "ENVIAR PARÁMETROS DEL REQUEST HTTP GET - URL");
-        
+
         return "params/index";
     }
 
@@ -21,6 +23,32 @@ public class EjemploParamsController {
     public String param(@RequestParam(name = "texto", defaultValue = "TEXTO POR DEFECTO") String texto, Model model){
         model.addAttribute("titulo", "RECIBIR PARÁMETROS DEL REQUEST HTTP GET - URL");
         model.addAttribute("resultado", "El texto enviado es :" + texto);
+
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+        model.addAttribute("titulo", "RECIBIR PARÁMETROS DEL REQUEST HTTP GET - URL");
+        model.addAttribute("resultado", "El saludo enviado es :" + saludo + "' y el número es '" + numero + "''");
+
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+            
+        } catch (NumberFormatException e) {
+            numero = 0;
+        }
+
+        model.addAttribute("titulo", "RECIBIR PARÁMETROS DEL REQUEST HTTP GET - URL");
+        model.addAttribute("resultado", "El saludo enviado es :" + saludo + "' y el número es '" + numero + "''");
 
         return "params/ver";
     }
