2bc3f331a738a42b80d4f8d6beb8c275932e7ca9
Julio-06 <jtcesarin1998@gmail.com>
2022-10-18 17:55:46 -0500

    [CLASE] 39. Ejemplo factura y relaciones entre sus componentes

---
+++ di/src/main/java/di/di/models/domain/IteamFactura.java
@@ -0,0 +1,19 @@
+package di.di.models.domain;
+
+public class IteamFactura {
+    private Producto producto;
+    private Integer cantidad;
+
+    public Producto getProducto() {
+        return producto;
+    }
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    public Integer getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
