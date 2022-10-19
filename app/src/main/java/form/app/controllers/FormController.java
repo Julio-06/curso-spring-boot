d89249838608fee3f773d9ba8c6be1db95c7c7fd
Julio-06 <jtcesarin1998@gmail.com>
2022-10-19 09:48:35 -0500

    [CLASE] 50. Creando la clase model del formulario.

--- app/src/main/java/form/app/controllers/FormController.java
+++ app/src/main/java/form/app/controllers/FormController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import form.app.models.domain.Usuario;
+
 @Controller
 public class FormController {
     
@@ -21,10 +23,15 @@ public class FormController {
             @RequestParam String email,
             @RequestParam String password
         ){
+        
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        usuario.setEmail(email);
+        usuario.setPassword(password);
+        
+        
         model.addAttribute("titulo", "resultado");
-        model.addAttribute("username", username);
-        model.addAttribute("email", email);
-        model.addAttribute("password", password);
+        model.addAttribute("usuario", usuario);
 
         return "resultado";
     }
