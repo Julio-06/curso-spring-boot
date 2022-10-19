953152c7abf4ccd82116578ad0edea44dcd0e768
Julio-06 <jtcesarin1998@gmail.com>
2022-10-19 10:33:26 -0500

    [CLASE] 55. El atributo objeto de thymeleaf en el elemento form

--- app/src/main/java/form/app/controllers/FormController.java
+++ app/src/main/java/form/app/controllers/FormController.java
@@ -1,7 +1,7 @@
 package form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+/* import java.util.HashMap;
+import java.util.Map; */
 
 import javax.validation.Valid;
 
@@ -33,12 +33,12 @@ public class FormController {
         ){
         
         if(result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
+            /* Map<String, String> errores = new HashMap<>();
             result.getFieldErrors().forEach(err -> {
                 errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
             });
 
-            model.addAttribute("error", errores);
+            model.addAttribute("error", errores); */
 
             return "form";
         }
