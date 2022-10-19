5f177f9856cd68c90c3fb3d08f1ef097b88b6c9a
Julio-06 <jtcesarin1998@gmail.com>
2022-10-19 09:42:34 -0500

    [CLASE] 49. Método handler para recibir y  procesar los datos enviados.

--- app/src/main/java/form/app/controllers/FormController.java
+++ app/src/main/java/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -14,7 +15,17 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(
+            Model model, 
+            @RequestParam String username,
+            @RequestParam String email,
+            @RequestParam String password
+        ){
+        model.addAttribute("titulo", "resultado");
+        model.addAttribute("username", username);
+        model.addAttribute("email", email);
+        model.addAttribute("password", password);
+
         return "resultado";
     }
 
