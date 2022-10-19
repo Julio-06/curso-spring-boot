f3d3ba887b93efb35d264d094e511f675ed90c99
Julio-06 <jtcesarin1998@gmail.com>
2022-10-19 10:03:27 -0500

    [CLASE] 53. Validación del formulario usando anotaciones

--- app/src/main/java/form/app/controllers/FormController.java
+++ app/src/main/java/form/app/controllers/FormController.java
@@ -1,7 +1,13 @@
 package form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +24,22 @@ public class FormController {
 
     @PostMapping("/form")
     public String procesar(
-            Model model, Usuario usuario
+            @Valid Usuario usuario, BindingResult result, Model model
             /* @RequestParam String username,
             @RequestParam String email,
             @RequestParam String password */
         ){
         
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+
+            model.addAttribute("error", errores);
+
+            return "form";
+        }
         /* Usuario usuario = new Usuario();
         usuario.setUsername(username);
         usuario.setEmail(email);
--- app/src/main/java/form/app/models/domain/Usuario.java
+++ app/src/main/java/form/app/models/domain/Usuario.java
@@ -1,8 +1,16 @@
 package form.app.models.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
+
+    @NotEmpty
     private String username;
+
+    @NotEmpty
     private String email;
+
+    @NotEmpty
     private String password;
 
     public String getUsername() {
