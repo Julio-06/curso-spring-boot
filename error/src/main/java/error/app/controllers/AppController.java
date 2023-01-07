e1c3e9cbd66165546a50b75a05ad8544a52c3929
Julio-06 <jtcesarin1998@gmail.com>
2023-01-07 18:41:09 -0500

    [CLASE] Personalizando página de error para la excepción NumberFormatException

--- error/src/main/java/error/app/controllers/AppController.java
+++ error/src/main/java/error/app/controllers/AppController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
     @GetMapping("/index")
     public String index() {
-        Integer num = 100/0;
+        //Integer num = 100/0;
+        Integer num = Integer.parseInt("s");
         return "index";
     }
     
