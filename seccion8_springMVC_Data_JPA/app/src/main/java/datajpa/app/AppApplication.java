e42dd86659fef39e9a3da540020ee3e28f512376
Julio Tejeira <julio.tejeira@procuraduria.gob.pa>
2023-01-13 17:01:20 -0500

    [CLASE] 154. inicializando directorio uploads automáticamente usando CommandLineRunner

--- seccion8_springMVC_Data_JPA/app/src/main/java/datajpa/app/AppApplication.java
+++ seccion8_springMVC_Data_JPA/app/src/main/java/datajpa/app/AppApplication.java
@@ -1,13 +1,27 @@
 package datajpa.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import datajpa.app.models.services.IUploadFileService;
+
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	@Autowired
+	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		
+		uploadFileService.init();
 	}
 
 }
