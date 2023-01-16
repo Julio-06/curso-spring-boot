81ff50885a509c7d1365a063ddfc4f26a0dab9de
Julio-06 <jtcesarin1998@gmail.com>
2023-01-15 21:46:15 -0500

    [CLASE] 163. Creando controlador FacturaController con la acción handler crear.

---
+++ seccion8_springMVC_Data_JPA/app/src/main/java/datajpa/app/controllers/FacturaController.java
@@ -0,0 +1,45 @@
+package datajpa.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import datajpa.app.models.entities.Cliente;
+import datajpa.app.models.entities.Factura;
+import datajpa.app.models.services.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura") //NO AYUDARA A MANTENER LA FACTURA QUE PERSISTA EN LA SESSIÓN DEL CLIENTE HASTA QUE ESTA SEA GUARDADA.
+public class FacturaController {
+    
+    @Autowired
+    private IClienteService clienteService;
+    @GetMapping("/form/{clienteId}")
+    public String crear(
+        @PathVariable(value = "clienteId") Long clienteId, 
+        Model model, 
+        RedirectAttributes flash
+    ){
+        
+        Cliente cliente = clienteService.findOne(clienteId);
+
+        if(cliente == null){
+            flash.addFlashAttribute("error", "El cliente no existe!");
+            return "redirect:/listar";
+        }
+
+        Factura factura = new Factura();
+        factura.setCliente(cliente);
+
+        model.addAttribute("factura", factura);
+        model.addAttribute("titulo", "Crear Factura");
+
+        return "factura/form";
+    }
+}
